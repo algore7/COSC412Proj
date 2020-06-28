798d1618cf5896c6351cf400adb79f64565cd955
thiri3377 <thirihsumyataung@aol.com>
2020-06-28 03:24:46 -0400

    winning status done

--- web-cosc412proj/src/components/TriviaGame.js
+++ web-cosc412proj/src/components/TriviaGame.js
@@ -12,33 +12,40 @@ export default class TriviaGame extends React.Component {
         }
     }
 
+    shuffle(array) {
+        let shuffleInt = Math.floor(Math.random() * Math.floor(100));
+        let startIndex = 0;
+        let i = 0;
+        while(i < shuffleInt){
+            if (startIndex + 1 == array.length) {
+                startIndex = 0;
+            }
+            let temp = array[startIndex];
+            array[startIndex] = array[startIndex+1];
+            array[startIndex+1] = temp;
+            startIndex++;
+            i++;
+        }
+        return array;
+    }
+
     componentDidMount() {
         fetch("http://localhost:8000/api/trivia_questions")
         .then(res => res.json())
         .then(result => {
-            let resultQuestions = [];
-            for(let question of result) {
-                question["choices"] = [question["answer"], ...(question["falseAnswers"]).split(",")];
-
-                //shuffle
-                let shuffleInt = Math.floor(Math.random() * Math.floor(100));
-                let startIndex = 0;
-                let i = 0;
-                while(i < shuffleInt){
-                    if (startIndex + 1 == question["choices"].length) {
-                        startIndex = 0;
-                    }
-                    let temp = question["choices"][startIndex];
-                    question["choices"][startIndex] = question["choices"][startIndex+1];
-                    question["choices"][startIndex+1] = temp;
-                    startIndex++;
-                    i++;
-                }
-                resultQuestions.push(question);
-            }
-            this.setState({questions: resultQuestions});
-            console.log(this.state.questions);
+            this.setState({questions: this.initQuestions(result)});
         });
+    }
+
+    initQuestions(result) {
+        let resultQuestions = [];
+        for(let question of result) {
+            question["choices"] = [question["answer"], ...(question["falseAnswers"]).split(",")];
+            question["choices"] = this.shuffle(question["choices"]);
+            resultQuestions.push(question);
+        }
+        resultQuestions = this.shuffle(resultQuestions);
+        return resultQuestions;
     }
 
     changeToHomepage = () => {
@@ -47,7 +54,9 @@ export default class TriviaGame extends React.Component {
 
     startGame = () => {
         this.setState({
-            status: "playing"
+            status: "playing",
+            questions: this.initQuestions(this.state.questions),
+            currentQuestionIndex: 0
         });
     }
 
@@ -56,7 +65,6 @@ export default class TriviaGame extends React.Component {
         if(currQuestion["choices"][number] === currQuestion["answer"]) {
             let nextNumber = this.state.currentQuestionIndex + 1;
             if (nextNumber === this.state.questions.length) {
-                this.setState({status: "won"});
                 return;
             }
             this.setState({currentQuestionIndex: nextNumber});
@@ -80,10 +88,17 @@ export default class TriviaGame extends React.Component {
             let currQuestion = this.state.questions[this.state.currentQuestionIndex];
             currComponent = <div>
                 <h1>{currQuestion["question"]}</h1>
-                <button onClick={() => {this.checkAnswer(0)}}>{currQuestion["choices"][0]}</button>
-                <button onClick={() => {this.checkAnswer(1)}}>{currQuestion["choices"][1]}</button>
-                <button onClick={() => {this.checkAnswer(2)}}>{currQuestion["choices"][2]}</button><br/>
-                <button onClick={() => {this.checkAnswer(3)}}>{currQuestion["choices"][3]}</button>
+                <button className="ans-btn" onClick={() => {this.checkAnswer(0)}}>{currQuestion["choices"][0]}</button>
+                <button className="ans-btn" onClick={() => {this.checkAnswer(1)}}>{currQuestion["choices"][1]}</button>
+                <button className="ans-btn" onClick={() => {this.checkAnswer(2)}}>{currQuestion["choices"][2]}</button><br/>
+                <button className="ans-btn" onClick={() => {this.checkAnswer(3)}}>{currQuestion["choices"][3]}</button>
+            </div>
+        }
+        else if(this.state.status === "lost") {
+            currComponent = <div>
+                <h1>Sorry :(, you lost....</h1>
+                <button className="initial-btn" onClick={this.changeToHomepage}>Back to homepage</button>
+                <button className="initial-btn" id="initial-play-btn"  onClick={this.startGame}>Try again?</button>
             </div>
         }
         
