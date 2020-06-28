e43e016b45c1d8cb9d79ef6e0042ad32895f66c7
thiri3377 <thirihsumyataung@aol.com>
2020-06-28 03:12:24 -0400

    trivia question rotation done

--- web-cosc412proj/src/components/TriviaGame.js
+++ web-cosc412proj/src/components/TriviaGame.js
@@ -7,7 +7,8 @@ export default class TriviaGame extends React.Component {
         super(props);
         this.state = {
             status: "initial",
-            questions: []
+            questions: [],
+            currentQuestionIndex: 0
         }
     }
 
@@ -15,7 +16,27 @@ export default class TriviaGame extends React.Component {
         fetch("http://localhost:8000/api/trivia_questions")
         .then(res => res.json())
         .then(result => {
-            this.setState({questions: result});
+            let resultQuestions = [];
+            for(let question of result) {
+                question["choices"] = [question["answer"], ...(question["falseAnswers"]).split(",")];
+
+                //shuffle
+                let shuffleInt = Math.floor(Math.random() * Math.floor(100));
+                let startIndex = 0;
+                let i = 0;
+                while(i < shuffleInt){
+                    if (startIndex + 1 == question["choices"].length) {
+                        startIndex = 0;
+                    }
+                    let temp = question["choices"][startIndex];
+                    question["choices"][startIndex] = question["choices"][startIndex+1];
+                    question["choices"][startIndex+1] = temp;
+                    startIndex++;
+                    i++;
+                }
+                resultQuestions.push(question);
+            }
+            this.setState({questions: resultQuestions});
             console.log(this.state.questions);
         });
     }
@@ -25,21 +46,52 @@ export default class TriviaGame extends React.Component {
     }
 
     startGame = () => {
-        
+        this.setState({
+            status: "playing"
+        });
+    }
+
+    checkAnswer = (number) => {
+        let currQuestion = this.state.questions[this.state.currentQuestionIndex];
+        if(currQuestion["choices"][number] === currQuestion["answer"]) {
+            let nextNumber = this.state.currentQuestionIndex + 1;
+            if (nextNumber === this.state.questions.length) {
+                this.setState({status: "won"});
+                return;
+            }
+            this.setState({currentQuestionIndex: nextNumber});
+        }
+        else {
+            this.setState({status: "lost"})
+        }
     }
 
     render() {
         let currComponent = null;
         if (this.state.status === "initial") {
-            currComponent = <Container fluid id="trivia-container">
-                                    <h1>Welcome to Trivia Game!</h1>
-                                    <br/>
-                                    <button className="initial-btn" onClick={this.changeToHomepage}>Back</button>
-                                    <button className="initial-btn" id="initial-play-btn"  onClick={this.startGame}>Play</button>
-                            </Container>
+            currComponent = <div>
+                <h1>Welcome to Trivia Game!</h1>
+                <br/>
+                <button className="initial-btn" onClick={this.changeToHomepage}>Back</button>
+                <button className="initial-btn" id="initial-play-btn"  onClick={this.startGame}>Play</button>
+            </div>
+        }
+        else if(this.state.status === "playing") {
+            let currQuestion = this.state.questions[this.state.currentQuestionIndex];
+            currComponent = <div>
+                <h1>{currQuestion["question"]}</h1>
+                <button onClick={() => {this.checkAnswer(0)}}>{currQuestion["choices"][0]}</button>
+                <button onClick={() => {this.checkAnswer(1)}}>{currQuestion["choices"][1]}</button>
+                <button onClick={() => {this.checkAnswer(2)}}>{currQuestion["choices"][2]}</button><br/>
+                <button onClick={() => {this.checkAnswer(3)}}>{currQuestion["choices"][3]}</button>
+            </div>
         }
         
 
-        return currComponent;
+        return  (
+            <Container fluid id="trivia-container">
+                {currComponent}
+            </Container>
+        );
     }
 }
