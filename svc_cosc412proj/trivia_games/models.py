61c860ea28409864ee6c1f74c765a927abc713be
thiri3377 <thirihsumyataung@aol.com>
2020-06-27 21:47:58 -0400

    trivia games API setup done

--- svc_cosc412proj/trivia_games/models.py
+++ svc_cosc412proj/trivia_games/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Trivia question
+class Question(models.Model):
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    falseAnswers = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.question}: {self.answer}"
